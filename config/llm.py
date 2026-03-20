9ec7b3d3c5751dc0d9e5ceb556c7b6ba125808a7
amitsingh2060 <amitsingh2060@yahoo.com>
2026-03-20 12:14:44 +0530

    fix

--- config/llm.py
+++ config/llm.py
@@ -1,4 +1,36 @@
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
+
+# def get_llm():
+#     return ChatOllama(model="llama3.1")
+
+# from dotenv import load_dotenv
+# import os
+
+# # Load environment variables
+# load_dotenv()
+
+# from langchain_openai import ChatOpenAI
+
+
+# def get_llm():
+#     return ChatOpenAI(
+#         model="gpt-4o-mini",
+#         temperature=0,
+#         api_key=os.getenv("OPENAI_API_KEY")  # optional but safer
+#     )
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from langchain_groq import ChatGroq
+
 
 def get_llm():
-    return ChatOllama(model="llama3.1")
+    return ChatGroq(
+        model="llama-3.1-8b-instant",   # fast + free
+        api_key=os.getenv("GROQ_API_KEY"),
+        temperature=0
+    )
